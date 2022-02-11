d6e82c8e11878568ea5830c510671ee9f810cfa3
Stephen Cronin <cronin4392@gmail.com>
2022-02-11 16:43:41 -0500

    Linting

--- src/scripts/Scenes/GameScene.js
+++ src/scripts/Scenes/GameScene.js
@@ -3,7 +3,6 @@ import Player from '../Sprites/Player';
 // import Coin from '../Sprites/Coin';
 import { colors } from '../constants';
 
-
 export default class GameScene extends Phaser.Scene {
   player;
   coin;
@@ -17,14 +16,20 @@ export default class GameScene extends Phaser.Scene {
   preload() {
     this.load.image('snake-body', new URL('../../assets/snake-body.png', import.meta.url).href);
     this.load.image('snake-food', new URL('../../assets/snake-food.png', import.meta.url).href);
-    this.load.image('background', new URL('../../assets/snake-background.png', import.meta.url).href);
+    this.load.image(
+      'background',
+      new URL('../../assets/snake-background.png', import.meta.url).href
+    );
   }
 
   create() {
-    this.background = this.add.image(this.game.config.width / 2, this.game.config.height / 2, 'background');
+    this.background = this.add.image(
+      this.game.config.width / 2,
+      this.game.config.height / 2,
+      'background'
+    );
     this.player = new Player(this, this.game.config.width / 2, this.game.config.height / 2);
     // this.coin = new Coin(this, this.game.config.width / 2, this.game.config.height / 2);
-
   }
 
   update(time) {
